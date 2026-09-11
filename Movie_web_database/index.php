724a703c8c0f01712bd1da49a39ececf315d7cb7
Viadd1 <146704370+Viadd1@users.noreply.github.com>
2026-09-11 11:40:25 +0200

    Started making the form

--- Movie_web_database/index.php
+++ Movie_web_database/index.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <form action="/insert.php" method="get">
+        <label for="name">Movie name:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="year">Year of release:</label><br>
+        <input type="number" id="year" name="year"><br>
+        <input type="submit">
+    <?php
+    echo "this will be a movie database soon!";
+    ?>
 
-<?php
-echo "hello";
-
-?>
+</html>
