5355fa1b9df00960e4087d7591e839b36510c947
sofiaengl <sofia.englundh.7850@student.uu.se>
2026-09-15 16:27:19 +0200

    Ändrade layouten på web och la till kommentarer

--- Movie_web_database/index.php
+++ Movie_web_database/index.php
@@ -2,13 +2,14 @@
 <html>
 <body>
     <h1>Movie Database</h1>
-    <p>Welcome! Fill in your review below</p>
-    <br>
+    <h4>Welcome! Fill in your review below</h4>
     <form action="/insert.php" method="get">
         <label for="name">Movie name:</label><br>
         <input type="text" id="name" name="name"><br>
+        <br>
         <label for="year">Year of release:</label><br>
         <input type="number" id="year" name="year"><br>
+        <br>
         <label for="genre">Movie genre:</label><br>
         <select name="genre" id="genre">
             <option value="comedy">Comedy</option>
@@ -16,11 +17,12 @@
             <option value="adventure">Adventure</option>
             <option value="fantasy">Fantasy/SciFi</option>
         </select><br>
+        <br>
         <label for="rating">Rating:</label><br>
         <input type="number" id="rating" name="rating" min="1" max="5"><br>
+        <br>
         <input type="submit"><br>
-    <?php
-    echo "this will be sent to the movie database when it is implemented!";
-    ?>
-
+        <br>
+        <h4>The entry will be sent to the movie database and can be viewed at showmovies.php!</h4>
+    
 </html>
--- Movie_web_database/showmovies.php
+++ Movie_web_database/showmovies.php
@@ -1,16 +1,52 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Movie Database</h1>
+    <h4>Below is a list of all movies that have been entered in the database up until this moment</h4>
+
+<!-- Below we make the design of the table:
+    100% means that it covers the entire screen
+    Padding 2px is the space between the cell edges and the cell content
+    nth-child(even) means style every other table row element -->
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  text-align: left;
+  padding: 2px;
+}
+
+tr:nth-child(even) {
+  background-color: #FF83c1;
+}
+</style>
+
+<!-- Below we create the table:
+    echo the table with each heading
+    we select all in the database under movies
+    then we echo what exists in the database in the table created -->
+
 <?php
-header("All movies in the database")
 
 include 'db.php';
 
-echo '<table><tr><th>mid</th><th>mname</th><th>myear</th><th>mgenreid</th><th>mrating</th></tr>';
+echo '<table><tr><th>Movie name</th><th>Name</th><th>Year of release</th><th>Genre ID</th><th>Rating</th></tr>';
 
 $sql = "SELECT * FROM `movies`";
 $result = $link->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<tr> <td>" . $row["mid"] . "</td><td>" . $row["mname"] . "</td><td>" . $row["myear"] . "</td><td>" . $row["mgenreid"] . "</td><td>" . $row["mrating"] . "</td></tr>";
+        echo 
+        "<tr> <td>" . $row["mid"] . 
+        "</td><td>" . $row["mname"] . 
+        "</td><td>" . $row["myear"] . 
+        "</td><td>" . $row["mgenreid"] . 
+        "</td><td>" . $row["mrating"] . 
+        "</td></tr>";
     }
 } else {
     echo "0 results";
@@ -18,3 +54,6 @@ if ($result->num_rows > 0) {
 
 echo '</table>';
 ?>
+
+<body>
+    <h4>This is the end of the list! Add more movies at index.php!</h4>
